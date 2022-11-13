3657092a3d64bee7ecbf5d6ada457930151786f3
uliana <kolosovichulia@mail.ru>
2022-11-13 19:30:00 +0300

    ulia k

--- src/app/aboutSite/aboutSite.component.ts
+++ src/app/aboutSite/aboutSite.component.ts
@@ -5,7 +5,6 @@ import {Component, OnInit} from '@angular/core';
   selector: 'app-main',
   templateUrl: './aboutSite.component.html',
   styleUrls: ['./aboutSite.component.css',
-    './assets/fonts/font-awesome.min.css',
   ]
 })
 export class AboutSiteComponent implements OnInit {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,8 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css',
-    'fonts/font-awesome.min.css'
+  styleUrls: ['./main.component.css'
   ]
 })
 export class MainComponent implements OnInit {
